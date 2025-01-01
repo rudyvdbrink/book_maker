b4f5a40e486582a4448a311365ce9524f63f684e
Rudy <30622165+rudyvdbrink@users.noreply.github.com>
2025-01-01 18:41:05 +0100

    update

--- return_book_text.py
+++ return_book_text.py
@@ -1,131 +1,19 @@
 
-# %%
-
-from audiobook_generator.config.general_config import GeneralConfig
-from audiobook_generator.book_parsers.epub_book_parser import EpubBookParser
-
-# %% define arguments class
-
-class Args:
-    def __init__(self):
-        self.input_file = 'path_to_your_epub_file.epub'
-        self.output_folder = 'output_directory'
-        self.preview = False
-        self.output_text = True
-        self.log = None
-        self.no_prompt = True
-        self.title_mode = 'auto'  # Options: 'auto', 'tag_text', 'first_few'
-        self.newline_mode = 'single'  # Options: 'single', 'double', 'none'
-        self.chapter_start = None
-        self.chapter_end = None
-        self.remove_endnotes = True
-        self.search_and_replace_file = None
-        self.tts = None
-        self.language = None
-        self.voice_name = None
-        self.output_format = None
-        self.model_name = None
-        self.break_duration = None
-        self.voice_rate = None
-        self.voice_volume = None
-        self.voice_pitch = None
-        self.proxy = None
-        self.piper_path = None
-        self.piper_speaker = None
-        self.piper_sentence_silence = None
-        self.piper_length_scale = None
-
-# %% parse the book
-
-args = Args()
-args.input_file = './examples/The_Life_and_Adventures_of_Robinson_Crusoe.epub'
-#args.input_file = './temp/The Colour Out of Space - HP Lovecraft.epub'
-config = GeneralConfig(args)
-
-# %%
-
-book = EpubBookParser(config)
-
-# %%
-
-# def generate_chapter_file(text):
-#     from TTS.api import TTS
-#     tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=True)
-
-#     # generate speech by cloning a voice using default settings
-#     tts.tts_to_file(text=text,
-#                     file_path="temp/chapter.wav",
-#                     speaker_wav="female_reference.wav",
-#                     language="en")
-    
-# # %%
-
-# generate_chapter_file(text)
-
-# %%
-
+# %% libraries
 import os
-from pydub import AudioSegment
-from TTS.api import TTS
 
-def split_text(text, max_length=250):
-    def split_chunk(chunk):
-        if len(chunk) <= max_length:
-            return [chunk]
-        mid = len(chunk) // 2
-        split_point = chunk.rfind(' ', 0, mid)
-        if split_point == -1:
-            split_point = chunk.find(' ', mid)
-        if split_point == -1:
-            return [chunk]
-        return split_chunk(chunk[:split_point].strip()) + split_chunk(chunk[split_point:].strip())
+from supporting_functions import book_parser, generate_chapter_file, convert_wav_to_mp3
 
-    chunks = []
-    for sentence in text.split('. '):
-        if len(sentence) > max_length:
-            for sub_chunk in sentence.split(', '):
-                chunks.extend(split_chunk(sub_chunk.strip()))
-        else:
-            chunks.append(sentence)
-    return chunks
+# %% get book text
 
-def generate_chapter_file(text, chapter_number, output_dir="temp"):
-    tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=True)
-    
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    
-    chunks = split_text(text)
-    audio_files = []
-    
-    for i, chunk in enumerate(chunks):
-        file_path = os.path.join(output_dir, f"chapter_{chapter_number}_part_{i}.wav")
-        tts.tts_to_file(text=chunk, file_path=file_path, speaker_wav="female_reference.wav", language="en")
-        audio_files.append(file_path)
-    
-    combined = AudioSegment.empty()
-    for file in audio_files:
-        combined += AudioSegment.from_wav(file)
-    
-    combined.export(os.path.join(output_dir, f"chapter_{chapter_number}_combined.wav"), format="wav")
+infile = './examples/The call of Cthulhu - HP Lovecraft.epub'
+book = book_parser(infile)
 
-    # Delete all chapter parts
-    for file in audio_files:
-        os.remove(file)
-
-# #delete all files with the word 'part' in the name
-# for file in os.listdir('temp'):
-#     if 'part' in file:
-#         os.remove(os.path.join('temp', file))
-
-#function to convert wav to mp3
-def convert_wav_to_mp3(wav_file, mp3_file):
-    AudioSegment.from_wav(wav_file).export(mp3_file, format="mp3")
-
-# %%
+#filter out empty chapters
+chapters = book.get_chapters(' ')
+chapters = [chapter for chapter in book.get_chapters(' ') if chapter != ('', '')]
 
 #generate chapter files
-chapters = book.get_chapters(' ')
 for chapter_number, chapter in enumerate(chapters):
     #skip this chapter if it exists already
     if os.path.exists(f'temp/chapter_{chapter_number}_combined.wav'):
@@ -133,15 +21,23 @@ for chapter_number, chapter in enumerate(chapters):
     text = chapter[1]  # Assuming chapter[1] contains the text of the chapter
     generate_chapter_file(text, chapter_number)
 
-#convert each chapter to mp3
-for file in os.listdir('temp'):
-    if file.startswith('chapter_') and file.endswith('.wav'):
-        convert_wav_to_mp3(os.path.join('temp', file), os.path.join('temp', file.replace('.wav', '.mp3')))
 
-# %%
+# Get book metadata
+book_author = book.get_book_author()
+book_title  = book.get_book_title()
+
+#convert each chapter to mp3 with metadata
+for chapter_number, chapter in enumerate(chapters):
+    chapter_title = chapter[0]
+    for file in os.listdir('temp'):
+        if file.startswith(f'chapter_{chapter_number}_') and file.endswith('.wav'):
+            mp3_file = file.replace('.wav', '.mp3').replace('_combined', '')
+            convert_wav_to_mp3(os.path.join('temp', file), os.path.join('temp', mp3_file), chapter_title, chapter_number, book_author, book_title)
+            
+# %% file cleanup and organization
 
 # create a folder with the book title in the 'books' folder
-output_folder = os.path.join('books', book.title)
+output_folder = os.path.join('books', book.get_book_title())
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
@@ -151,6 +47,6 @@ for file in os.listdir('temp'):
         os.rename(os.path.join('temp', file), os.path.join(output_folder, file))
 
 #remove chapter files from the temp folder
-for file in os.listdir('temp'):
-    if file.startswith('chapter_'):
-        os.remove(os.path.join('temp', file))
+# for file in os.listdir('temp'):
+#     if file.startswith('chapter_'):
+#         os.remove(os.path.join('temp', file))
--- supporting_functions.py
+++ supporting_functions.py
@@ -1,7 +1,13 @@
 # %%
 
+import os
+from pydub import AudioSegment
+from TTS.api import TTS
+
 from audiobook_generator.config.general_config import GeneralConfig
 from audiobook_generator.book_parsers.epub_book_parser import EpubBookParser
+
+from mutagen.easyid3 import EasyID3
 
 # %% book parsing
 
@@ -35,11 +41,84 @@ class Args:
         self.piper_length_scale = None
 
 def book_parser(infile):
+    # infile = './examples/The_Life_and_Adventures_of_Robinson_Crusoe.epub'
+    # book = book_parser(infile)
     args = Args(infile)
     config = GeneralConfig(args)
     book = EpubBookParser(config)
     return book
 
-# %%
+# %% text to speech
 
-infile = './examples/The_Life_and_Adventures_of_Robinson_Crusoe.epub'
+def split_text(text, max_length=250):
+    def split_chunk(chunk):
+        if len(chunk) <= max_length:
+            return [chunk]
+        mid = len(chunk) // 2
+        split_point = chunk.rfind(' ', 0, mid)
+        if split_point == -1:
+            split_point = chunk.find(' ', mid)
+        if split_point == -1:
+            return [chunk]
+        return split_chunk(chunk[:split_point].strip()) + split_chunk(chunk[split_point:].strip())
+
+    chunks = []
+    for sentence in text.split('. '):
+        if len(sentence) > max_length:
+            for sub_chunk in sentence.split(', '):
+                chunks.extend(split_chunk(sub_chunk.strip()))
+        else:
+            chunks.append(sentence)
+    return chunks
+
+def generate_chapter_file(text, chapter_number, output_dir="temp"):
+    tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=True)
+    
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    
+    chunks = split_text(text)
+    audio_files = []
+    
+    for i, chunk in enumerate(chunks):
+        file_path = os.path.join(output_dir, f"chapter_{chapter_number}_part_{i}.wav")
+        #skip if text is empty
+        if not chunk.strip():
+            continue
+        tts.tts_to_file(text=chunk, file_path=file_path, speaker_wav="female_reference.wav", language="en")
+        
+
+        audio_files.append(file_path)
+    
+    combined = AudioSegment.empty()
+    for file in audio_files:
+        combined += AudioSegment.from_wav(file)
+    
+    combined.export(os.path.join(output_dir, f"chapter_{chapter_number}_combined.wav"), format="wav")
+
+    # Delete all chapter parts
+    for file in audio_files:
+        os.remove(file)
+
+# #delete all files with the word 'part' in the name
+# for file in os.listdir('temp'):
+#     if 'part' in file:
+#         os.remove(os.path.join('temp', file))
+
+# %% file conversion
+
+def convert_wav_to_mp3(wav_file, mp3_file, chapter_title, chapter_number, artist, album):
+    audio = AudioSegment.from_wav(wav_file)
+    audio.export(mp3_file, format="mp3", tags={
+        "title": chapter_title,
+        "tracknumber": str(chapter_number),
+        "artist": artist,
+        "album": album
+    })
+    # Add additional metadata using mutagen
+    audiofile = EasyID3(mp3_file)
+    audiofile["title"] = chapter_title
+    audiofile["tracknumber"] = str(chapter_number)
+    audiofile["artist"] = artist
+    audiofile["album"] = album
+    audiofile.save()
